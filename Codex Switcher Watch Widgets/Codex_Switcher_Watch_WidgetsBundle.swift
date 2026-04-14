e6da34dc46f9ebdec09e663df483eb5a951195f2
Marcel Kwiatkowski <marcel2215@icloud.com>
2026-04-14 02:38:02 +0200

    Add watch widgets target

---
+++ Codex Switcher Watch Widgets/Codex_Switcher_Watch_WidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Codex_Switcher_Watch_WidgetsBundle.swift
+//  Codex Switcher Watch Widgets
+//
+//  Created by Marcel Kwiatkowski on 2026-04-14.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Codex_Switcher_Watch_WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Codex_Switcher_Watch_Widgets()
+    }
+}
